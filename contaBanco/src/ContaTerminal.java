2fcbbcdde3a802658281424bf9e6b07da995b841
amandakelly3 <amandakelly.tj@gmail.com>
2023-10-06 14:45:27 -0300

    Commit contaterminal

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("Olá, digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("digite o número da sua agência: ");
+        int agencia = entrada.nextInt();
+
+        System.out.println("digite o número da sua conta: ");
+        int numero = entrada.nextInt();
+
+        System.out.println("digite o saldo da sua conta: ");
+        double saldo = entrada.nextDouble();
+
+        System.out.println("Olá " + nome + ", Sua agência é " + agencia + ", sua conta é " + numero + " e seu saldo é " + saldo + ".");
+        entrada.close();
+
+}
+}
+
